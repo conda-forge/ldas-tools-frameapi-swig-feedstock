44aca8941f9c8a56767d1adcbb3be50b56a2b3fc
Duncan Macleod <macleoddm@cardiff.ac.uk>
2026-07-01 10:57:53 +0100

    chore: update build script syntax

--- recipe/build-python.sh
+++ recipe/build-python.sh
@@ -8,23 +8,20 @@ cd ${_builddir}
 
 # configure
 cmake \
-	${SRC_DIR} \
-	${CMAKE_ARGS} \
-	-DBUILD_TESTING:BOOL=no \
-	-DCMAKE_BUILD_TYPE:STRING=Release \
-	-DCMAKE_OSX_ARCHITECTURES:STRING="${OSX_ARCH}" \
-	-DCMAKE_POLICY_VERSION_MINIMUM:STRING=3.5 \
-	-DENABLE_SWIG_PYTHON2:BOOL=no \
-	-DENABLE_SWIG_PYTHON3:BOOL=yes \
-	-DPYTHON3_EXECUTABLE:FILE="${PYTHON}" \
-	-DPYTHON3_VERSION:STRING=${PY_VER} \
-	-DSWIG_EXECUTABLE:FILE="${BUILD_PREFIX}/bin/swig" \
+  ${CMAKE_ARGS} \
+  -DBUILD_TESTING:BOOL=no \
+  -DCMAKE_OSX_ARCHITECTURES:STRING="${OSX_ARCH}" \
+  -DENABLE_SWIG_PYTHON3:BOOL=yes \
+  -DPYTHON3_EXECUTABLE:FILE="${PYTHON}" \
+  -DPYTHON3_VERSION:STRING=${PY_VER} \
+  -DSWIG_EXECUTABLE:FILE="${BUILD_PREFIX}/bin/swig" \
+  ${SRC_DIR} \
 ;
 
 # override the PYTHON3_LIBRARIES cache variable to stop
 # attempting to link against the static libpython library
 if [[ "${target_platform}" == "linux"* ]]; then
-	cmake -DPYTHON3_LIBRARIES="" ${SRC_DIR}
+  cmake -DPYTHON3_LIBRARIES="" ${SRC_DIR}
 fi
 
 # build
@@ -32,7 +29,7 @@ cmake --build python --parallel ${CPU_COUNT} --verbose
 
 # check
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
-	ctest --parallel ${CPU_COUNT} --verbose
+  ctest --parallel ${CPU_COUNT} --verbose
 fi
 
 # install
